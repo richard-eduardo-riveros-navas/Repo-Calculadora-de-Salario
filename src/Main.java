eda973cc8581454f476a67603a96bf44f8ca87a2
Mariana Mestra Padilla <mmestrap1@unicartagena.edu.co>
2026-06-09 15:40:13 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        CalculadoraSalarioCli calculadora = new CalculadoraSalarioCli();
+        calculadora.iniciar();
     }
 }
