4828632ab40520b9a4ba26a79381790c1ce2b88c
Abraham-Puello <apuellor@unicartagena.edu.co>
2026-06-10 13:57:33 -0500

    revisión

---
+++ src/CalculadoraSalarioCli.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class CalculadoraSalarioCli {
+
+    Scanner scanner = new Scanner(System.in);
+    CalculadoraSalario calc = new CalculadoraSalario();
+
+    public void iniciar() {
+        int opcion;
+
+        do {
+            System.out.println("=== Calculadora de Salario ===");
+            System.out.println("1. Calcular salario base");
+            System.out.println("2. Calcular pago por horas extra");
+            System.out.println("3. Calcular descuentos");
+            System.out.println("4. Calcular salario neto");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+
+                    System.out.print("Ingrese la cantidad de horas trabajadas: ");
+                    double h = scanner.nextDouble();
+                    System.out.print("Ingrese el valor de la hora trabajada: ");
+                    double v = scanner.nextDouble();
+
+                    double resultado = calc.calcularSalarioBase(v, h);
+
+                    System.out.println("El salario base es: " + resultado);
+                    break;
+                case 2:
+                    System.out.print("Ingrese la cantidad de horas extra: ");
+                    int horas = scanner.nextInt();
+
+                    System.out.print("Valor por hora: ");
+                    double valor = scanner.nextDouble();
+
+                    System.out.println("El pago por horas extra es: " + calc.calcularHorasExtra(horas, valor));
+                    break;
+                case 3:
+                    System.out.print("Ingrese el salario base: ");
+                    double salarioBase = scanner.nextDouble();
+
+                    System.out.println("Los descuentos son: "
+                            + calc.calcularDescuentos(salarioBase));
+                    break;
+
+                case 4:
+                    System.out.print("Salario base: ");
+                    double salarioBase = scanner.nextDouble();
+
+                    System.out.print("Pago por horas extra: ");
+                    double pagoExtras = scanner.nextDouble();
+
+                    System.out.print("Descuentos: ");
+                    double descuentos = scanner.nextDouble();
+
+                    System.out.println(
+                            "El salario neto es: " + calc.calcularSalarioNeto(salarioBase, pagoExtras, descuentos));
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Selecciona una opción valida");
+            }
+
+            System.out.println();
+
+        } while (opcion != 5);
+    }
+}
